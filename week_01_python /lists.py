d679b637807e08a54107edc0dce319d83c94db7d
Ms. Persaud <85592940+learncs0@users.noreply.github.com>
2021-09-19 17:46:28 +0000

    starting python list practice

---
+++ week_01_python /lists.py
@@ -0,0 +1,21 @@
+
+#create list of students in 701
+students_701 = ["Efrain", "Emely", "Joseph", "Samantha", "Leynin", "Kasey", "Elder", "Kevin", "Carlos", "Kennedi"]
+
+#print list of students in 701
+print(students_701)
+
+#output:
+#['Efrain', 'Emely', 'Joseph', 'Samantha', 'Leynin', 'Kasey', 'Elder', 'Kevin', 'Carlos', 'Kennedi']
+
+#print length of list
+print (len(students_701))
+#output:  10
+
+#access elements of list
+print(students_701[-1])
+print(students_701[0])
+
+#output
+#Kennedi
+#Efrain
