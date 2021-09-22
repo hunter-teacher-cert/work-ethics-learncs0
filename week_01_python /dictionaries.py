d1d695f1c1d6da4f1f9809d8e23ec20e6f6507a8
Ms. Persaud <85592940+learncs0@users.noreply.github.com>
2021-09-22 17:51:20 +0000

    updated

--- week_01_python /dictionaries.py
+++ week_01_python /dictionaries.py
@@ -16,3 +16,16 @@ eng2spn['keep'] = 'Mantén'
 rosters = {'701class':30, '702class':35, '703class':25, '704class':6}
 for akey in rosters.keys():  
     print("Got key", akey, "which maps to value", rosters[akey])
+#output
+#Got key 701class which maps to value 30
+#Got key 702class which maps to value 35
+#Got key 703class which maps to value 25
+#Got key 704class which maps to value 6
+
+
+cr = list(rosters.keys())       # Make a list of all of the keys
+print(cr)
+print(cr[3]) 
+
+#['701class', '702class', '703class', '704class']
+704class
