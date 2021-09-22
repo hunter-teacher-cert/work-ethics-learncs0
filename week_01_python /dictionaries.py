588ba1495a8a39b682b1c06a410fb66fc70bfbc0
Ms. Persaud <85592940+learncs0@users.noreply.github.com>
2021-09-22 17:45:22 +0000

    started dictionaries

---
+++ week_01_python /dictionaries.py
@@ -0,0 +1,18 @@
+
+#did Eng-Spn to help with
+#translations at school
+
+eng2spn = {}
+eng2spn['pretest'] = 'prueba preliminar'
+eng2spn['sheet'] = 'la hoja'
+eng2spn['learning target'] = 'objetivo de aprendizaje'
+eng2spn['match it'] = 'compárelo'
+eng2spn['were you'] = 'estuviste'
+eng2spn['fingers'] = 'dedos'
+eng2spn['keep'] = 'Mantén'
+
+#delete key-value 
+
+rosters = {'701class':30, '702class':35, '703class':25, '704class':6}
+for akey in rosters.keys():  
+    print("Got key", akey, "which maps to value", rosters[akey])
